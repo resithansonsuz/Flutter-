cc5a8940dc015dc8e8143be4d0d634a66495157c
Ömer Reşithan Sonsuz <45916117+resithansonsuz@users.noreply.github.com>
2022-01-01 17:52:53 +0300

    Add files via upload
    Box Decoration

--- main.dart
+++ main.dart
@@ -1,20 +1,12 @@
-/*Flutter_Widget_MaterialApp_Scaffold_FloatingActionButton_ContainerWidget_ContainerAlignment_Padding_Margin*/
-
-
-
-
-
-
-
-
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final String _img1 =
+      'https://images.unsplash.com/photo-1640768239132-4a0c74249b61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +16,43 @@ class MyApp extends StatelessWidget {
         /* backgroundColor: Colors.yellow, */
         appBar:
             AppBar(title: const Text('Başlık'), backgroundColor: Colors.black),
-        body:
-              Center(
-                child: Container(
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(60),
+            child: const Text(
+              'Resithan',
+              style: TextStyle(fontSize: 60),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              shape: BoxShape.rectangle,
+              border: Border.all(
+                width: 5,
+                color: Colors.black,
+              ),
+              borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(60),
+                  bottomRight: Radius.circular(60),
+                  topLeft: Radius.circular(60),
+                  topRight: Radius.circular(60)),
+              image: DecorationImage(
+                image: NetworkImage(_img1),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.yellow,
+                    offset: Offset(20, -20),
+                    blurRadius: 20),
+                BoxShadow(
+                    color: Colors.yellow,
+                    offset: Offset(-20, 20),
+                    blurRadius: 30),
+              ],
+            ),
+          ),
+        ),
+        /*  child: Container(
                   
                   color: Colors.blueGrey,
                   width: 300,
@@ -40,9 +66,9 @@ class MyApp extends StatelessWidget {
                     height: 100,
                     child: const Text("TheAnswer"),
                   ),
-                ),
-              ),
-         /* Container(
+                ), */
+
+        /* Container(
           color: Colors.red,
           width: 100,
           height: 200,
@@ -55,6 +81,7 @@ class MyApp extends StatelessWidget {
           /* constraints: const BoxConstraints(
               minWidth: 100, minHeight: 100, maxHeight: 200, maxWidth: 200), */
         ), */
+
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             debugPrint("Tıklandı");
